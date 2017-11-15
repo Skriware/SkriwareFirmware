0c5ef7f3f42e6e705ef9ed43bfdfcc40d6ddd31c
sztojfen <stankiewicz.stefan@gmail.com>
2017-11-15 15:52:53 +0100

    Update Configuration.h
    Leveling matrix size change from 5x3 to 5x5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -865,13 +865,13 @@
 
   // Set the number of grid points per dimension.
   #define GRID_MAX_POINTS_X 5
-  #define GRID_MAX_POINTS_Y 3//GRID_MAX_POINTS_X
+  #define GRID_MAX_POINTS_Y 5//GRID_MAX_POINTS_X
 
   // Set the boundaries for probing (where the probe can reach).
   #define LEFT_PROBE_BED_POSITION 18
   #define RIGHT_PROBE_BED_POSITION 180
   #define FRONT_PROBE_BED_POSITION 32
-  #define BACK_PROBE_BED_POSITION 270
+  #define BACK_PROBE_BED_POSITION 272
 
   // The Z probe minimum outer margin (to validate G29 parameters).
   #define MIN_PROBE_EDGE 0
