972f23262aba37aa3814000f6b0e6564381d981c
Lukkoz <ukikoza@gmail.com>
2020-08-27 12:01:18 +0200

    1.1.9.4

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -36,7 +36,7 @@
    * Marlin release version identifier
    */
 
-  #define SHORT_BUILD_VERSION "1.1.9.3"
+  #define SHORT_BUILD_VERSION "1.1.9.4"
 
   /**
    * Verbose version identifier which should contain a reference to the location
